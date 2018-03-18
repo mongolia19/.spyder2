f7e5dfaf7bcd7b8d5c47f223235ec9298cbf6e5b
mongolia19 <mongolia19@gmail.com>
2018-03-18 17:06:14 +0900

    loop all stock to test r2 score

--- getFinaceData.py
+++ getFinaceData.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import tushare as ts
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import AdaBoostClassifier
@@ -44,7 +44,7 @@ def get_data_and_label(stock_num, start_date, end_date):
         result_list.append(tup[0])
     a_few_day_list = list()
     label_list = list()
-    rim = 5
+    rim = 10
     for i in range(rim, len(extended_list)):
         total_one = list()
         for j in range(rim - 1):
@@ -82,12 +82,17 @@ def get_data_and_label(stock_num, start_date, end_date):
     return extended_list, result_list
 def get_data_and_label_for_regressor(stock_num, start_date, end_date):
     e = ts.get_hist_data(stock_num,start=start_date,end=end_date)
+    if e is None:
+        return None,None
+    # print( e['close'])
     data = list()
     for i in e.iterrows():
         # print( i)
         # print (i[-1].values)
-        # data.append((i[-1].close,i[-1].high,i[-1].low,i[-1].open,i[-1].ma5,i[-1].ma10,i[-1].ma20))
-        data.append((i[-1].close,i[-1].high,i[-1].low,i[-1].open,i[-1].volume))
+        data.append((i[-1].close,i[-1].high,i[-1].low,i[-1].open,i[-1].ma5,i[-1].ma10,i[-1].ma20,i[-1].high-i[-1].low))
+        # data.append((i[-1].close,i[-1].high,i[-1].low,i[-1].open,i[-1].volume))
+        # data.append((i[-1].close, i[-1].high, i[-1].low, i[-1].open))
+    data.reverse()
     BIGUP = 2
     UP = 1
     DOWN = -1
@@ -114,29 +119,7 @@ def get_data_and_label_for_regressor(stock_num, start_date, end_date):
     label_list.pop(0)
     del data, result_list, extended_list
     return a_few_day_list, label_list
-    exit(-1)
-    for d in range(len(data)):
-        if d>1:
-            print(type(data[d]))
-            print(data[d])
-            combined = np.append(data[d],data[d-1])
-            combined = np.append(combined,data[d-2])
-            extended_list.append(combined)
-        else:
-            combined = np.append(data[d],data[d])
-            combined = np.append(combined,data[d])
-            extended_list.append(combined)
-        if d+1<len(data):
-            if data[d+1][1]>data[d][1] and data[d+1][1]-data[d][1]:
-                result_list.append(UP)
-            else:
-                result_list.append(DOWN)
-        else:
-            result_list.append(DOWN)
-    del data
-    print( len(extended_list))
-    print (len(result_list))
-    return extended_list, result_list
+
 def get_data_and_label_2day(stock_num, start_date, end_date):
     e = ts.get_hist_data(stock_num,start=start_date,end=end_date)
     data = list()
@@ -203,62 +186,54 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+from sklearn.metrics import r2_score
+symbol_dict=ts.get_stock_basics()
+stock_list = ['601878','601881','600606']
+stock_r2_list = list()
+for index in symbol_dict.index:
+    print("training on stock " + index)
+    stockindex = index
+    extended_list, result_list = get_data_and_label_for_regressor(stockindex, '2016-06-23', '2017-07-30')
+    if result_list is None or extended_list is None or len(result_list)<50 or len(extended_list)<50:
+        continue
+    increaseCount = 0
+    X, y = extended_list, result_list
+    X = preprocessing.scale(X)
+    y = preprocessing.scale(y)
 
-stockindex = '000001'
-extended_list, result_list = get_data_and_label_for_regressor(stockindex, '2001-06-23', '2016-07-30')
-tot = len(result_list)
-increaseCount = 0
-for i in result_list:
-    if i == 1:
-        increaseCount += 1
-print("total count is "+ str(tot )+ " increase count is " +str(increaseCount) + " rate is " + str(increaseCount/tot))
-    # e = ts.get_hist_data('000002',start='2014-06-23',end='2017-07-30')
-X, y = extended_list, result_list
-X = StandardScaler().fit_transform(X)
-# data set
+    # y = StandardScaler().fit_transform(y)
+    # data set
 
-test_list, test_label_list = get_data_and_label_for_regressor(stockindex, '2017-08-01', '2018-02-23')
-test_list = StandardScaler().fit_transform(test_list)
-    # e = ts.get_hist_data('000002',start='2014-06-23',end='2017-07-30')
-# X, y = test_list, test_label_list
-from sklearn import metrics
-from sklearn.neighbors import KNeighborsClassifier
-from plot_report import main_plot
-# fit a k-nearest neighbor model to the data
-# model = KNeighborsClassifier()
-# model.fit(X, y)
-# clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2,random_state = 0)
-# scores = cross_val_score(clf, X, y)
-# print "decision tree : ", scores.mean()
-# test_result = model.predict(test_list)
-# print ("KNeighborsClassifier on test: ", classification_report(test_label_list,test_result))
-# score
-# num_trees = 460
-# clf = RandomForestClassifier(n_estimators=num_trees, max_depth=None,min_samples_split = 2, random_state = 0,max_features='sqrt', n_jobs=4)
-# # 定义一个随机森林分类器
-# scores = cross_val_score(clf, X, y)
-# print "random forest : ", scores.mean()
+    test_list, test_label_list = get_data_and_label_for_regressor(stockindex, '2017-08-01', '2018-03-17')
+    if  test_list is None or  len(test_list)<50:
+        continue
+    test_list = preprocessing.scale(test_list)
+    test_label_list = preprocessing.scale(test_label_list)
+    # 这里是随机森林训练器的模型精确度得分
+    from sklearn import svm
+    parameters = {'kernel':('linear', 'rbf'), 'C':[1, 2, 4], 'gamma':[0.125, 0.25, 0.5 ,1, 2, 4]}
 
-# 这里是随机森林训练器的模型精确度得分
-from sklearn import svm
-parameters = {'kernel':('linear', 'rbf'), 'C':[1, 2, 4], 'gamma':[0.125, 0.25, 0.5 ,1, 2, 4]}
+    svr = svm.SVR()  # class
+    clf = GridSearchCV(svr, parameters, n_jobs=1)
+    clf.fit(X, y)  # training the svc model
+    cv_result = pd.DataFrame.from_dict(clf.cv_results_)
+    with open('cv_result.csv', 'w') as f:
+        cv_result.to_csv(f)
+    print('The parameters of the best model are: ')
+    print(clf.best_params_)
+    # y_pred = clf.predict(iris.data)
+    # clf = ExtraTreesClassifier(n_estimators=num_trees, max_depth=None,min_samples_split = 2, random_state = 0)
+    test_result = clf.predict(test_list)
+    # print ("svm on test: \r\n", classification_report(test_label_list,test_result))
+    # print(mean_squared_error(test_label_list,test_result))
+    temp_r2_score = r2_score(test_label_list, test_result, multioutput='variance_weighted')
+    print(temp_r2_score)
+    stock_r2_list.append((index,temp_r2_score))
 
-svr = svm.SVR()  # class
-clf = GridSearchCV(svr, parameters, n_jobs=1)
-clf.fit(X, y)  # training the svc model
-cv_result = pd.DataFrame.from_dict(clf.cv_results_)
-with open('cv_result.csv', 'w') as f:
-    cv_result.to_csv(f)
-print('The parameters of the best model are: ')
-print(clf.best_params_)
-# y_pred = clf.predict(iris.data)
-# clf = ExtraTreesClassifier(n_estimators=num_trees, max_depth=None,min_samples_split = 2, random_state = 0)
-
-scores = cross_val_score(clf, X, y)
-print ("svm : ", scores.mean())
-test_result = clf.predict(test_list)
-# print ("svm on test: \r\n", classification_report(test_label_list,test_result))
-
+stock_r2_list = sorted(stock_r2_list,key=lambda x:x[1],reverse=True)
+print( "stock r2 list: ", stock_r2_list)
 showTestWithLabel(test_result, test_label_list)
 
 exit(0)
@@ -267,14 +242,24 @@ exit(0)
 # report = classification_report(test_label_list,test_result)
 #
 # print "gradient_boosting_classifier on test: \r\n", classification_report(test_label_list,test_result)
-for i in range(10,100):
+score_tupleList = list()
+for i in range(100,800,100):
     print('ploting '+ str(i)+ " tree forest...")
-    model = gradient_boosting_classifier(X, y, 20000)
-    bdt = AdaBoostClassifier(model,
-                             algorithm="SAMME",
-                             n_estimators=i)
-
-    bdt.fit(X, y)
-    test_result = bdt.predict(test_list)
-    report_str = str(classification_report(test_label_list, test_result))
-    main_plot(report_str, './pics/'+ str(i) + 'tree.png')
+    bdt = RandomForestRegressor(n_estimators=i*10, criterion='mse', random_state=1, n_jobs=-1)
+    net = AdaBoostRegressor(bdt,
+     n_estimators=2)
+    net.fit(X, y)
+    test_result = net.predict(test_list)
+    # report_str = str(classification_report(test_label_list, test_result))
+    # main_plot(report_str, './pics/'+ str(i) + 'tree.png')
+    # mse_score = mean_squared_error(test_label_list, test_result)
+    mse_score = r2_score(test_label_list, test_result,multioutput='variance_weighted')
+    score_tupleList.append((i,mse_score))
+score_tupleList = sorted(score_tupleList,key=lambda x:x[1],reverse=True)
+print(score_tupleList)
+bdt = RandomForestRegressor(n_estimators=score_tupleList[0][0], criterion='mse', random_state=1, n_jobs=-1)
+net = AdaBoostRegressor(bdt,
+                        n_estimators=2)
+net.fit(X, y)
+test_result = net.predict(test_list)
+showTestWithLabel(test_result, test_label_list)
